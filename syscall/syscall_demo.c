0cccc2d87f33aa9b29559f3b2803f30e54bfa9e2
Prasad Anil Tatkare <prasad.tatkare895@gmail.com>
2020-10-19 20:46:26 +0530

    System call demo

---
+++ syscall/syscall_demo.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char const *argv[]){
+
+    int fd;
+    fd = open("desd.txt", O_WRONLY | O_CREAT);
+    
+    if(-1 == fd){
+        perror("Error in open");
+        return -1;
+    }
+    
+    write(fd,"Prasad",6);
+    close(fd);
+    return 0;
+}
