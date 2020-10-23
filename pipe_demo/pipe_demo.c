b2e416b6d447c9bcf9b5df288164e54426e4e4a8
Prasad Anil Tatkare <prasad.tatkare895@gmail.com>
2020-10-23 15:32:15 +0530

    Demo for Pipe IPC

--- pipe_demo/pipe_demo.c
+++ pipe_demo/pipe_demo.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main(int argc, char const *argv[]){
 
-    pfd[1]
+    unsigned char read_buffer[128];
+    pid_t id;
+    int pfd[2];     //Array of two integers
+    pipe(pfd);      //Pipe is created
+
+    id = fork();         //Child process is created using fork
+    if(0 == id){
+        //Child Process
+        read(pfd[0],read_buffer,128);
+        printf("In Child: %s\n", read_buffer);
+        close(pfd[0]);
+    }else{
+        //Parent Process
+        write(pfd[1],"CDAC\n",5);
+        close(pfd[1]);
+    }
     return 0;
 }
