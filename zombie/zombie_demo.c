b178074e9cef7957dc9ab0b44df6e7238862a1d5
Prasad Anil Tatkare <prasad.tatkare895@gmail.com>
2020-11-02 14:31:40 +0530

    Demo of zombie and inherit

---
+++ zombie/zombie_demo.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+
+
+int main(int argc, char const *argv[]){
+    pid_t id;
+
+    id = fork();
+    if(0 == id){
+        //Child
+        printf("Child started\n");
+        sleep(2);
+        printf("Child exited\n");
+    }else{
+
+        printf("Parent started\n");
+        sleep(5);
+        printf("Parent exited\n");
+    }
+
+    return 0;
+}
